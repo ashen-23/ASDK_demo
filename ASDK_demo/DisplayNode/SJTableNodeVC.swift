e886ec0f0eb06e105758ea3f11de63ddf6233503
shmily <shmilyshijian@foxmail.com>
2017-08-02 18:12:54 +0800

    add table header

--- ASDK_demo/DisplayNode/SJTableNodeVC.swift
+++ ASDK_demo/DisplayNode/SJTableNodeVC.swift
@@ -18,9 +18,9 @@ class SJTableNodeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configTable()
-        
         grade = SJClass.create()
+
+        configTable()        
     }
 
     
@@ -28,7 +28,10 @@ class SJTableNodeVC: UIViewController {
     
         tableNode = ASTableNode()
         
-        tableNode?.frame = SJScreenRect
+        tableNode?.frame = UIScreen.main.bounds
+        
+        tableNode?.delegate = self
+        tableNode?.dataSource = self
         
         view.addSubnode(tableNode!)
     }
@@ -48,6 +51,115 @@ extension SJTableNodeVC: ASTableDelegate, ASTableDataSource {
         return grade?[section].persons.count ?? 0
     }
     
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        
+        let person = grade?[indexPath.section].persons[indexPath.row]
+        
+        return {
+        
+            SJTableNoe(person: person)
+        }
+    }
+    
+    // header view
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let aTitle = grade?[section].name
+        return SJableHeaderNode(title: aTitle).view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 45
+    }
+}
+
+//*****************************************
+//MARK: - table node
+fileprivate class SJTableNoe: ASCellNode {
+    
+    lazy var imgNode = ASNetworkImageNode()
+    lazy var textNode = ASTextNode()
+    
+    var person: SJPerson?
+    
+    init(person: SJPerson?) {
+        super.init()
+        
+        self.person = person
+        
+        initUI()
+    }
+    
+    override func didLoad() {
+        super.didLoad()
+        
+        let aTap = UITapGestureRecognizer(target: self, action: #selector(SJTableNoe.doTapAvator))
+        imgNode.view.addGestureRecognizer(aTap)
+    }
+    
+    func initUI() {
+    
+        selectionStyle = .none
+        
+        imgNode.style.preferredSize = CGSize(width: 50, height: 50)
+        imgNode.defaultImage = UIImage(named: "default") // 默认图
+        imgNode.isUserInteractionEnabled = true
+        imgNode.url = person?.avator
+        let width = 50 * UIScreen.main.scale
+        
+        imgNode.imageModificationBlock = { image in
+            
+            let rect = CGRect(x: 0, y: 0, width: width, height: width)
+            UIGraphicsBeginImageContextWithOptions(image.size, false, UIScreen.main.scale)
+            UIBezierPath(roundedRect: rect, cornerRadius: width / 2).addClip() // 圆角
+            
+            image.draw(in: rect)
+            let modifiedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            return modifiedImage
+        }
+        
+        addSubnode(imgNode) // 必须要添加
+        
+        textNode.attributedText = NSAttributedString(string: person?.name ?? "", attributes: defaultAttri())
+        addSubnode(textNode)
+    }
+    
+    func doTapAvator() {
+    
+        self.view.makeToast((person?.name ?? "") + "is selected")
+    }
+    
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        let stack = ASStackLayoutSpec(direction: .horizontal, spacing: 20, justifyContent: .start, alignItems: .center, children: [imgNode, textNode])
+        
+        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20), child: stack)
+    }
+}
+
+
+class SJableHeaderNode: ASDisplayNode {
+    
+    lazy var textNode = ASTextNode()
+    
+     init(title: String?) {
+        super.init()
+        
+        textNode.attributedText = NSAttributedString(string: title ?? "", attributes: [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 18), NSForegroundColorAttributeName: UIColor.black])
+        
+        addSubnode(textNode)
+        
+        backgroundColor = UIColor.white
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: 5, left: 20, bottom: 5, right: 20), child: textNode)
+    }
 }
 
 //*****************************************
@@ -62,7 +174,7 @@ fileprivate struct SJClass {
         name = randomName()
         
         persons = [SJPerson]()
-        let randon = Int(arc4random_uniform(30) + 1)
+        let randon = Int(arc4random_uniform(30) + 2)
         for _ in 0..<randon {
             persons.append(SJPerson())
         }
@@ -72,7 +184,7 @@ fileprivate struct SJClass {
     
         var classes = [SJClass]()
         
-        let randon = Int(arc4random_uniform(6) + 1)
+        let randon = Int(arc4random_uniform(6) + 10)
         for _ in 0..<randon {
         
             classes.append(SJClass())

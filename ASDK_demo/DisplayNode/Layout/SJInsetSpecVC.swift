a832dcfa82ad7a36f639dafa023aae511742d27e
shmily <shmilyshijian@foxmail.com>
2017-08-03 17:39:45 +0800

    inset

--- ASDK_demo/DisplayNode/Layout/SJInsetSpecVC.swift
+++ ASDK_demo/DisplayNode/Layout/SJInsetSpecVC.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import AsyncDisplayKit
 
 class SJInsetSpecVC: UIViewController {
 
+    lazy fileprivate var insetNode = InsetSpecNode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        insetNode.frame = CGRect(x: 50, y: 100, width: 300, height: 300)
+        view.addSubnode(insetNode)
     }
 
+}
 
+fileprivate class InsetSpecNode: ASDisplayNode {
+
+    lazy var aNode = ASDisplayNode()
+    
+    override init() {
+        
+        super.init()
+        
+        initUI()
+    }
+    
+    func initUI() {
+    
+        aNode.backgroundColor = UIColor.purple
+        addSubnode(aNode)
+        
+        backgroundColor = SJColor(230, green: 230, blue: 230)
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        
+        let inset = UIEdgeInsets(top: 20, left: 35, bottom: 20, right: 30)
+        return ASInsetLayoutSpec(insets: inset, child: aNode)
+    }
 }
